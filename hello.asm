ecf98a7a0a028c1af5ee3537aa28b59a9574cbe4
Tabura <wuttichai3113@gmail.com>
2022-02-27 23:52:29 +0700

    fix: asm format

--- hello.asm
+++ hello.asm
@@ -1,16 +1,17 @@
-		global 	_start
+	global 	_start
 
-		section .text
-_start: mov 	rax, 1     		    ; system call for write
-		mov 	rdi, 1   	        ; file handle 1 is stdout
-		mov 	rsi, msg 		    ; address of string to output
-		mov 	rdx, msglen 	    ; sizeof("Hello, World!\n")
-		syscall
+	section .text
+_start: 
+	mov		rax, 1     		    ; system call for write
+	mov		rdi, 1   	        ; file handle 1 is stdout
+	mov		rsi, msg 		    ; address of string to output
+	mov		rdx, msglen 	    ; sizeof("Hello, World!\n")
+	syscall
 
-		mov 	rax, 60		        ; system call for exit
-		xor 	rdi, rdi		    ; exit code 0
-		syscall   
+	mov		rax, 60		        ; system call for exit
+	xor		rdi, rdi		    ; exit code 0
+	syscall   
 
-		section .data
+	section .data
 msg: 	db		"Hello, World!", 10 ; 10 is '\n' (ascii)
-msglen: equ 	$ - msg
+msglen: equ		$ - msg
